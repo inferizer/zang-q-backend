556b3d1620b2ec5f06985ece5c246c73e392c926
inferizer <codecamp.1412@gmail.com>
2023-11-06 14:19:49 +0700

    Merge branch 'develop' of https://github.com/inferizer/zang-q-backend into develop

--- src/app.js
+++ src/app.js
@@ -3,26 +3,92 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const authRoute = require("./route/auth-router");
-const vendorRoute = require('./route/vendor-route')
-const testRoute = require('./route/test')
-const adminRoute = require('./route/admin-route')
+const vendorRoute = require("./route/vendor-route");
+const testRoute = require("./route/test");
+const adminRoute = require("./route/admin-route");
 const notFoundMiddleware = require("./middlewares/not-founded");
 const errorMiddleware = require("./middlewares/error");
 const PORT = process.env.PORT || "5000";
-
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  },
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+// socket.io >>>>>>>>>>>>>>>>>>
+io.on("connect", (socket) => {
+  console.log(`>User<: ${socket.id} connected`);
+  socket.on("create-room", (roomInfo) => {
+    socket.join(roomInfo);
+  });
+
+  socket.on("join room", (roomInfo) => {
+    socket.join(roomInfo);
+    console.log(`>User<: ${socket.id} Joined room: ${roomInfo}`);
+  });
+
+  socket.on("booking", (data) => {
+    console.log(data);
+    io.to(`${data.userId}`).to(`${data.shopName}`).emit(
+      "ticket",
+      // (mocking) DB_reservation
+      {
+        data: "Online Queue Detail",
+        id: data.userId,
+        name: data.name,
+        qNumber: 2,
+        //   date: dayjs().format("DD MMMM YYYY"),
+        //   time: dayjs().format("h:mm A"),
+      }
+    );
+  });
+
+  socket.on("booking for customer", (onstieData) => {
+    io.to(`${onstieData.shopName}`).emit(
+      "onsite queue",
+      // (mocking) DB_reservation
+      {
+        data: "Onsite Queue",
+        id: onstieData.userId,
+        name: onstieData.name,
+        // mobile: mobileFormat(onstieData.mobile),
+        qNumber: 3,
+        // date: dayjs().format("DD MMMM YYYY"),
+        // time: dayjs().format("h:mm A"),
+      }
+    );
+  });
+
+  //!!cancel booking and delete DB_reservation
+  socket.on("cancel", (cancelInfo) => {
+    io.to(`${cancelInfo.shopName}`).emit("cancel queue", {
+      userId: cancelInfo.userId,
+    });
+  });
+
+  //disconnect status
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
+});
+// socket.io >>>>>>>>>>>>>>>>>>
+
 app.use("/auth", authRoute);
 app.use("/vendor", vendorRoute);
-app.use('/test',testRoute)
-app.use('/admin',adminRoute)
+app.use("/test", testRoute);
+app.use("/admin", adminRoute);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
-app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
+server.listen(PORT, () => console.log(`server running on port: ${PORT}`));
--- src/controllers/admin-controller.js
+++ src/controllers/admin-controller.js
@@ -1,29 +1,32 @@
-const bcrypt = require('bcryptjs');
-const createToken = require("../utils/jwt")
-const { adminRegisterSchema, adminLoginSchema } = require('../validator/admin-validator');
-const prisma = require('../models/prisma');
+const bcrypt = require("bcryptjs");
+const {
+  adminRegisterSchema,
+  adminLoginSchema,
+} = require("../validator/admin-validator");
+const prisma = require("../models/prisma");
+const createToken = require("../utils/jwt");
 
 exports.register = async (req, res, next) => {
   try {
     const { value, error } = adminRegisterSchema.validate(req.body);
     if (error) {
-      return next(error)
-
-    } console.log(value)
+      return next(error);
+    }
+    console.log(value);
     value.password = await bcrypt.hash(value.password, 10);
 
     const user = await prisma.users.create({
-      data: { ...value, role: "admin" }
-    })
+      data: { ...value, role: "admin" },
+    });
 
-    const payload = { userId: user.id, };
-    const accessToken =  createToken(payload)
+    const payload = { userId: user.id };
+    const accessToken = createToken(payload);
     delete user.password;
-    res.status(200).json({ accessToken, message: 'admin registered' });
+    res.status(200).json({ accessToken, msg: "ADMIN!!!!" });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
 exports.login = async (req, res, next) => {
   try {
@@ -33,19 +36,19 @@ exports.login = async (req, res, next) => {
     }
     const user = await prisma.users.findFirst({
       where: {
-        username: value.username
-      }
-    })
+        username: value.username,
+      },
+    });
     if (!user) {
-      return next(createError('invalid Login', 400));
+      return next(createError("invalid Login", 400));
     }
-    
-    const compareMatch = await bcrypt.compare(value.password, user.password)
+    console.log(user);
+    const compareMatch = await bcrypt.compare(value.password, user.password);
     if (!compareMatch) {
-      return next(createError('invalid Login', 400));
+      return next(createError("invalid Login", 400));
     }
     const payload = { userId: user.id };
-    const accessToken = createToken(payload)
+    const accessToken = createToken(payload);
 
     delete user.password;
     res.status(200).json({ accessToken, msg: "Welcome Admin!!!" });
@@ -58,16 +61,16 @@ exports.getAdmin = async (req, res, next) => {
   try {
     const user = await prisma.users.findFirst({
       where: {
-        id: req.user.id
-      }
-    })
-    res.status(200).json({ user })
+        id: req.user.id,
+      },
+    });
+    res.status(200).json({ user });
   } catch (err) {
-    next(err)
+    next(err);
   }
 };
 
-// find 
+// find
 
 exports.find_All_Shop = async (req, res, next) => {
   try {
@@ -90,36 +93,37 @@ exports.find_All_Shop = async (req, res, next) => {
         isOpen: true,
         isApprove: true,
         shopAccount: true,
-        shopAccountId: true
-      }
-    })
-    res.status(200).json({ shopData })
+        shopAccountId: true,
+      },
+    });
+    res.status(200).json({ shopData });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 exports.approved = async (req, res, next) => {
   try {
-    const { id } = req.body
+    const { id } = req.body;
     const approveShop = await prisma.shops.findMany({
       select: {
         id: true,
         isApprove: true,
-     }
-    })
+      },
+    });
     await prisma.shops.update({
-      where : {
-        shops : id},
-      data : {
-        isApprove: "approved"
-      }
-    })
-    console.log(shops)
-    res.status(200).json({ approveShop })
+      where: {
+        shops: id,
+      },
+      data: {
+        isApprove: "approved",
+      },
+    });
+    console.log(shops);
+    res.status(200).json({ approveShop });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
 // exports.reject  //delete
 // exports.approve //update
